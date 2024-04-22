424e98e3b12d065d11dc27201d61b9e0d6aecd48
Luc van Soest <lvsoest@gmail.com>
2024-04-22 13:16:32 +0200

    Update simple-interest.sh
    Update username

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <your GitHub username>
+# lucvansoest
 # Input:
 # p, principal amount
 # t, time period in years
